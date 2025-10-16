bc8b20f4cb91568465b0623850233c66b9f09529
Abhishek Keri <keri.abhi9@gmail.com>
2025-10-15 20:19:46 -0500

    animated progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,21 +1,58 @@
+import { useEffect, useState } from "react";
+
 const MIN = 0;
 const MAX = 100;
 
-export default function ProgressBar({ value }: { value: number }) {
+export function ProgressBar({
+  value,
+  onComplete,
+}: {
+  value: number;
+  onComplete: () => void;
+}) {
   const clampedValue = Math.min(Math.max(value, MIN), MAX);
+  const [percent, setPercent] = useState(0);
+
+  useEffect(() => {
+    setPercent(clampedValue);
+
+    if (clampedValue >= MAX) {
+      onComplete();
+    }
+  }, [clampedValue]);
 
   return (
     <div className="progress">
+      <span style={{ color: percent > 49 ? "white" : "black" }}>
+        {percent.toFixed(0)}%
+      </span>
       <div
-        className="progress-bar"
-        style={{ width: `${clampedValue}%` }}
+        style={{ transform: `translateX(${percent - MAX}%)` }}
         role="progressbar"
-        aria-valuenow={clampedValue}
+        aria-valuenow={percent}
         aria-valuemin={MIN}
         aria-valuemax={MAX}
-      >
-        {clampedValue}%
-      </div>
+      />
+    </div>
+  );
+}
+
+export function AnimatedProgressBar() {
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((value) => value + 0.1);
+    }, 20);
+  }, []);
+
+  return (
+    <div>
+      <ProgressBar value={value} onComplete={() => setSuccess(true)} />
+      <span className="progress-state">
+        {success ? "Complete" : "Loading..."}
+      </span>
     </div>
   );
 }
